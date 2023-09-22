8ade6873b134efc0482f77749fb5d1a9c25f8d02
GeraM94 <gerardo.martinez2094@gmail.com>
2023-09-21 21:34:44 -0600

    part1.1

---
+++ src/components/Content.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Total from './Total'
+
+
+function Content({part1,part2,part3,exercises1,exercises2,exercises3}) {
+
+  return (
+    <>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+
+    </>
+  )
+}
+
+export default Content
