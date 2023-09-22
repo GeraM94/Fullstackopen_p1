5cc1b088a322288d84c6a47fb1221eec112870c5
GeraM94 <gerardo.martinez2094@gmail.com>
2023-09-22 01:38:30 -0600

    part1.8

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,16 @@
+const Statistics = (props) => { 
+    const { good, neutral, bad } = props
+    return (
+      <div>
+        <h1>Statics</h1>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
+        <div>all {good+neutral+bad}</div>
+        <div>average {isNaN((good-bad)/(good+neutral+bad)) ? 0 : (good-bad)/(good+neutral+bad)}</div>
+        <div>positive {isNaN((good)/(good+neutral+bad)) ? 0 : (good)/(good+neutral+bad)}</div>
+      </div>
+    )
+  }
+
+export default Statistics;
